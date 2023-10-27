5aa853bf60d7693599505f318e5c3e69747966ea
Artur Patoka <artur0patoka@gmail.com>
2023-10-27 11:01:33 +0200

    Lots of minor improvments

--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ import os
 import platform
 import re
 import subprocess
-import sys
 from typing import List, Literal, Dict, Optional
 
 import openai
+from yaspin import yaspin
+from yaspin.spinners import Spinners
 
 
 @dataclasses.dataclass
@@ -37,15 +38,17 @@ help_refusal_text = "I don't know how to help with that"
 
 
 class ChatWithMemory:
-    def __init__(self, api_key: str, system_prompt: str, temperature: float, functions: List = None,
+    def __init__(self, system_prompt: str, temperature: float, functions: List = None,
                  model: str = "gpt-3.5-turbo"):
-        self.api_key = api_key
         self.model = model
         self.temperature = temperature
         # TODO extract functions list just from the name using signature and docstring
         self.functions = default_functions if not functions else functions
         self.messages: Optional[List[Dict[str, str]]] = None
         self.system_prompt = system_prompt
+
+        self.generated_one_liners = []
+        self.executed_one_liners = []
 
     def initial_call(self):
         """
@@ -57,15 +60,27 @@ class ChatWithMemory:
         # Give the GPT some context about the system operated:
         self.messages.append(ChatMessage("system", f"System info: {self.get_system_info()}").__dict__)
 
-        response = openai.ChatCompletion.create(
-            model=self.model,
-            messages=self.messages,
-            temperature=self.temperature,
-            ## JUST TESTING
-            # functions=self.functions,
-            # function_call="none"
-        )
-        # todo check response
+        with yaspin(Spinners.flip, text="Initiating GPT...") as sp:
+            response = openai.ChatCompletion.create(
+                model=self.model,
+                messages=self.messages,
+                temperature=self.temperature,
+                # functions=self.functions,  # to be enabled with functions
+            )
+            # todo check response
+
+    def _handle_chat_completion_call(self, prompt, role: str = "user"):
+        with yaspin(Spinners.flip, text="Coming up with a nice answer...") as sp:
+            os_name = platform.system() if platform.system() != "Darwin" else "Mac OS"
+            prompt = f"Generate a terminal one-liner to {prompt} in {os_name}"
+            self.messages.append(ChatMessage(role, prompt).__dict__)
+            response = openai.ChatCompletion.create(
+                model=self.model,
+                messages=self.messages,
+                temperature=self.temperature,
+                # functions=self.functions,  # to be enabled with functions
+            )
+        return response
 
     @staticmethod
     def get_system_info():
@@ -80,21 +95,9 @@ class ChatWithMemory:
         }
         return system_info
 
-    def _handle_chat_completion_call(self, prompt, role: str = "user"):
-        os_name = platform.system() if platform.system() != "Darwin" else "Mac OS"
-        prompt = f"Generate a terminal one-liner to {prompt} in {os_name}"
-        self.messages.append(ChatMessage(role, prompt).__dict__)
-        response = openai.ChatCompletion.create(
-            model=self.model,
-            messages=self.messages,
-            temperature=self.temperature,
-            # functions=self.functions,
-            ## JUST TESTING
-            # function_call="none"
-        )
-        return response
-
     def ask_gpt(self, query):
+        # TODO: consider including pwd in a call
+
         # If not initialized, make the initial call
         # We want to make it here to minimize startup time
         if not self.messages:
@@ -102,12 +105,6 @@ class ChatWithMemory:
 
         # Ask the question
         response = self._handle_chat_completion_call(query)
-        # response = openai.ChatCompletion.create(
-        #     model=self.model,
-        #     messages=self.messages,
-        #     temperature=self.temperature,
-        #     functions=self.functions,)
-        #     # function_call="auto")
         response_message = response["choices"][0]["message"]
 
         # Check if GPT wanted to call a function
@@ -148,19 +145,36 @@ class ChatWithMemory:
         try:
             extracted_code = extract_code_snipped(resp)
             print(extracted_code)
-            if get_y_n("Execute? (y/n)"):
-                self.execute(extracted_code)
+            self.generated_one_liners.append(extracted_code)
             return extract_code_snipped(resp)
         except ValueError:
             print("No code returned")
 
-    @staticmethod
-    def execute(command):
+    def fresh_start(self):
+        self.initial_call()
+
+    def print_executed_one_liners(self) -> None:
+        print("Executed one-liners:")
+        for line in self.executed_one_liners:
+            print(line)
+
+    def print_generated_one_liners(self) -> None:
+        print("All generated one-liners:")
+        for line in self.generated_one_liners:
+            print(line)
+
+    def execute(self, command):
+        self.executed_one_liners.append(command)
         shell_output = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
         print(shell_output.stdout.decode("utf-8"))
 
 
 def check_if_file_exists(file_path: str):
+    """
+    To be used by the gpt as an AI function. Currently, disabled due to problems with hallucinated calls.
+    :param file_path:
+    :return:
+    """
     return str(os.path.exists(file_path))
 
 
@@ -168,20 +182,13 @@ def extract_code_snipped(gpt_answer: str) -> str:
     re_pattern = r"```.*\n*(.*)\n*```"
     finds = re.search(re_pattern, gpt_answer)
     if not finds:
+        # despite the initial prompt, sometimes the answer is given as a single line, containing only code
+        if len(gpt_answer.split("\n")) == 1 and help_refusal_text not in gpt_answer:
+            # in such case, we will only accept it if there are no new lines and the answer does not contain
+            # refusal to provide answer
+            return gpt_answer
+
+        # Nothing found and the answer is not a bare one-liner:
         print(gpt_answer)
         raise ValueError("No code found")
-    # assert len(finds) == 1, "More than one code snipped found, bad bad GPT"
     return finds.group(1)
-
-
-def get_y_n(input_text):
-    yes = {'yes', 'y', 'ye', ''}
-    no = {'no', 'n'}
-
-    choice = input(input_text).lower()
-    if choice in yes:
-        return True
-    elif choice in no:
-        return False
-    else:
-        sys.stdout.write("Please respond with 'yes' or 'no'")
