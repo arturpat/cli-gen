025c11b0c09d3d3b6f731bb5b76bfc6ac85e8f06
Artur Patoka <artur.patoka@captivateiq.com>
2023-10-27 10:59:47 +0200

    Refactored a lot of mess, improved user input handling

--- cli-gen.py
+++ cli-gen.py
@@ -1,5 +1,5 @@
 import os
-import re
+import sys
 
 import openai
 
@@ -14,61 +14,83 @@ system_prompt = ("You are a terminal one-liner generator. Always encapsulate gen
 # in case of hallucinating function names (like "python") add "Only use the functions you have been provided with."
 
 
-API_KEY = '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'
-temperature = 0.5
+temperature = 0.2
 
-openai.api_key = API_KEY
+decisions = {
+    "yes": {'yes', 'y', 'ye', ''},
+    "no": {'no', 'n'},
+    "fresh_start": {'f', 'fr', 'fresh'},
+    "quit": {'q', 'Q', 'quit'},
+    "history": {'h', 'hist', 'history'},
+    "new": {"n", "new"},
+    "retry": {"r", "retry"}
+}
+
+
+def get_api_key() -> str:
+    # check from env vars
+    key = os.environ.get("OPENAI_API_KEY")
+    if key:
+        return key
+
+    # check in the file
+    current_directory = os.path.dirname(os.path.abspath(__file__))  # cli-gen.py location
+    try:
+        with open(os.path.join(current_directory, "open_ai_api_key.txt")) as api_key_file:
+            key = api_key_file.readlines()[0].strip("\n")
+            return key
+    except FileNotFoundError:
+        pass  # no file with expected name
+    except IndexError:
+        pass  # empty file
+
+    # not found in file, not found in env vars
+    print("API key not found in env vars (searched for OPENAI_API_KEY) or in a file.")
+    print("Go to Go to https://platform.openai.com/account/api-keys to generate one for yourself")
+    print("OpenAI offers a free $5 worth credits for everyone once.")
+    print("Either paste the key here to store it next to cli-gen.py or save it as OPENAI_API_KEY yourself.")
+    key = input("Key: ")
+    # set up the key for now
+    os.environ["OPENAI_API_KEY"] = key
+
+    # dump the key
+
+    with open(os.path.join(current_directory, "open_ai_api_key.txt"), 'w') as text_file:
+        print(key, file=text_file)
+    print(f"Thanks, api key saved at {os.path.join(current_directory, 'open_ai_api_key.txt')}\n")
+
+    return key
+    # if not present, either ask to fill in or link to openai api tokens
 
 
 def main():
-    chat = ChatWithMemory(api_key=API_KEY, system_prompt=system_prompt, temperature=temperature)
+    openai.api_key = get_api_key()
+    chat = ChatWithMemory(system_prompt=system_prompt, temperature=temperature)
+    prompt = ' '.join(sys.argv)
+    command = chat.ask_gpt_code_snippet_only(prompt)
     while True:
-        resp = chat.ask_gpt_code_snippet_only(input("User\t: "))
-    # functions = [
-    #     {
-    #         "name": "check_if_file_exists",
-    #         "description": "Checks if a file with the given path exists",
-    #         "parameters": {
-    #             "type": "object",
-    #             "properties": {
-    #                 "file_path": {
-    #                     "type": "string",
-    #                     "description": "Path to the file in question",
-    #                 },
-    #             },
-    #             "required": ["file_path"],
-    #         },
-    #     }
-    # ]
-    #
-    # # The initial system message:
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[{
-    #         "role": "system",
-    #         "content": system_prompt
-    #     }],
-    #     temperature=temperature,
-    #     functions=functions)
-    #
-    # while True:
-    #     user_input = input("User\t:")
-    #
-    #     response = openai.ChatCompletion.create(
-    #         model="gpt-3.5-turbo",
-    #         messages=[{
-    #                 "role": "system",
-    #                 "content": system_prompt
-    #             },
-    #             {
-    #                 "role": "user",
-    #                 "content": user_input
-    #             }
-    #         ]
-    #     )
-    #
-    #     response_message = response['choices'][0]['message']['content']
-    #     print('AI: ', response_message)
+        choice = input("Execute? (Y)es/(R)etry/(N)ew prompt within the same conversation/(Q)uit ").lower()
+        if choice in decisions["yes"]:
+            chat.execute(command)
+            break
+        elif choice in decisions["new"]:
+            prompt = input("Enter new prompt: ")
+            command = chat.ask_gpt_code_snippet_only(prompt)
+            continue
+        elif choice in decisions["retry"]:
+            # TODO update prompts
+            command = chat.ask_gpt_code_snippet_only(prompt)
+            continue
+        elif choice in decisions["quit"]:
+            break
+        else:
+            sys.stdout.write("Provide a proper response...")
+
 
 if __name__ == '__main__':
     main()
+
+# TODO: clean up
+# TODO: add tracking history
+# TODO: add pretty colors
+# TODO: add asking for openai api key
