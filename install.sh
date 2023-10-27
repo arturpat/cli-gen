ee5cd70b997b71b3b78153cf15d3a8c0313b89cb
Artur Patoka <artur0patoka@gmail.com>
2023-10-27 12:26:20 +0200

    Major improvments to the install script

--- install.sh
+++ install.sh
@@ -6,22 +6,39 @@ PYTHON_PATH="$(pwd)/.venv/bin/python"
 CLI_GEN_PATH="$(pwd)/cli-gen.py"
 
 # Get the current shell
-current_shell=$(basename "$SHELL")
+current_shell=$(basename "$SHELL");
+echo "Current shell: $current_shell";
+echo "Python path: $PYTHON_PATH";
+echo "cli-gen path: $CLI_GEN_PATH";
+
 
 # Check if it's zsh
-if [ "$current_shell" == "zsh" ]; then
-    echo "alias cli-gen=\"$PYTHON_PATH $CLI_GEN_PATH" >> ~/.zshrc
+if [ "$current_shell" = "zsh" ]; then
+  if grep -q "alias cli-gen=" ~/.zshrc; then
+      echo "Alias cli-gen already exists in ~/.zshrc"
+  else
+      echo "alias cli-gen=\"$PYTHON_PATH $CLI_GEN_PATH\"" >> ~/.zshrc
+  fi
 
 # Check if it's bash
-elif [ "$current_shell" == "bash" ]; then
-    echo "alias cli-gen=\"$PYTHON_PATH $CLI_GEN_PATH" >> ~/.bashrc
+elif [ "$current_shell" = "bash" ]; then
+  if grep -q "alias cli-gen=" ~/.bashrc; then
+      echo "Alias cli-gen already exists in ~/.bashrc"
+  else
+      echo "alias cli-gen=\"$PYTHON_PATH $CLI_GEN_PATH\"" >> ~/.bashrc
+  fi
 
 # Check if it's fish
-elif [ "$current_shell" == "fish" ]; then
-    echo 'function cli-gen' >> ~/.config/fish/config.fish
-    echo "    $PYTHON_PATH ~/path/to/main.py \$argv" >> ~/.config/fish/config.fish
-    echo 'end' >> ~/.config/fish/config.fish
-    echo 'funcsave cli-gen' >> ~/.config/fish/config.fish
+elif [ "$current_shell" = "fish" ]; then
+  if grep -q "function cli-gen" ~/.config/fish/config.fish; then
+      echo "Function cli-gen already exists in ~/.config/fish/config.fish"
+  else
+      echo 'function cli-gen' >> ~/.config/fish/config.fish
+      echo "    $PYTHON_PATH $CLI_GEN_PATH \$argv" >> ~/.config/fish/config.fish
+      echo 'end' >> ~/.config/fish/config.fish
+      echo 'funcsave cli-gen' >> ~/.config/fish/config.fish
+  fi
 else
     echo "Unsupported shell: $current_shell"
 fi
+echo "Restart your terminal and then just use 'cli-gen your query'"
